345d40e11dc821b42b6da62695731983e5047f9f
VitaliiKosyk <kostamtik@gmail.com>
2022-02-24 00:28:48 +0200

    edit dop_tasks

--- dop_tasks/task_3_argparse.py
+++ dop_tasks/task_3_argparse.py
@@ -21,18 +21,28 @@ python task_1_ex_3.py 1+5-2
 Hint: use argparse module for parsing arguments from CLI
 """
 
-import  argparse
+import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('formula', nargs='*')
+parser.add_argument('user_input', type=str, help="formula")
 
 
-
+def check_formula(user_input):
+    """
+    :param user_input:
+    :return:
+    """
+    x = user_input.replace('-', '+-').split('+')
+    try:
+        result = sum(int(i) for i in x)
+    except ValueError:
+        return False, None
+    return True, result
 
 
 def main():
     args = parser.parse_args()
-    print(' Result:', )
+    print(check_formula(args.user_input))
 
 
 if __name__ == '__main__':
