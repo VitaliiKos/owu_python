99f52630ce43d87428c88adbeaddbead2d3b4f2a
VitaliiKosyk <kostamtik@gmail.com>
2022-03-15 11:19:31 +0200

    edit lesson 4

--- lesson_4/lesson_4_hw/task_1.py
+++ lesson_4/lesson_4_hw/task_1.py
@@ -1,9 +1,11 @@
+import sys
+
 try:
-    with open("emails.txt", encoding="UTF-8") as file_emails:
+    with open("emailqs.txt", encoding="UTF-8") as file_emails:
         with open("result.txt", "a", encoding="UTF-8") as f:
             for line in file_emails:
                 if line.strip().endswith('@gmail.com'):
                     print(line.split()[-1])
                     f.writelines(line.split()[-1] + '\n')
-except IOError:
-    print("An IOError has occurred!")
+except Exception:
+    print(sys.exc_info()[1])
